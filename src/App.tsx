e829eeb2db8ee16262a4c90b03c40c44021d7410
Dior99 <n.shaldaev@yandex.ru>
2021-04-19 17:55:23 +0300

    Added new file TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import './App.css';
+import TodoList from "./TodoList";
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 function App() {
+
+    const task01 = [
+        {id: 1, title: "HTML", isDone: true},
+        {id: 2, title: "CSS", isDone: true},
+        {id: 3, title: "REACT", isDone: false},
+        {id: 4, title: "JS", isDone: false},
+    ]
+
+    const task02 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am happy", isDone: true},
+        {id: 3, title: "Yo", isDone: false},
+        {id: 4, title: "Yes", isDone: false},
+    ]
+
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title={"Todo 01"} tasks={task01}/>
+            <TodoList title={"Todo 02"} tasks={task02}/>
         </div>
     );
 }
